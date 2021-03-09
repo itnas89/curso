574e16551a7ea1bf232836176ca6ff224e3d0000
Santi Paños <santi.panos@grupoicsolutions.com>
2021-03-09 10:46:13 +0100

    Tema 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Articulo from './Components/Articulo'
+import Bitcoin from './Components/Bitcoin'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-content">
+        <Articulo />
+        <Bitcoin />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/Components/Carga.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carga = (props) => {
+    return (
+        <ul>
+            <li>Carga rápida en estación: { props.carga }</li>
+        </ul>
+    )
+}
+
+export default Carga
