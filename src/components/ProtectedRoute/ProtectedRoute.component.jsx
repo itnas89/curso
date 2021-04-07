f19f6d1b7d41c53fb6b4ba0649ea59f3126ac03c
Santi Paños <santi.panos@grupoicsolutions.com>
2021-04-07 10:55:58 +0200

    merge

--- src/components/ProtectedRoute/ProtectedRoute.component.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.component.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-
-export const ProtectedRoute = ({ component: Component, ...rest }) => {
-=======
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { useSelector } from 'react-redux'
  
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
     const isLogged = useSelector(state => state.isLogged)
->>>>>>> origin/tema_8
     return (
         <Route
             {...rest}
@@ -23,8 +16,4 @@ export const ProtectedRoute = ({ component: Component, ...rest }) => {
             }}
         />
     );
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> origin/tema_8
